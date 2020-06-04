942e0f63d638d9616dd78497a369c4971845806d
Kirity Mahanty <kirity.mahanty@thoughtworks.com>
2020-06-04 21:18:25 +0530

    added OCL_API_HOST for the backend integration

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start the deploy.."
-
+OCL_API_HOST="$1"
 echo "Stop the running container.."
 docker stop ocl-frontend
 
@@ -9,7 +9,7 @@ echo "Remove the stopped container.."
 docker rm ocl-frontend
 
 echo "Run the latest docker.."
-docker run --name=ocl-frontend --restart unless-stopped -d -p 80:80 485402728093.dkr.ecr.ap-southeast-1.amazonaws.com/bahmni-msf/ocl-frontend:latest
+docker run --name=ocl-frontend --restart unless-stopped -d -p 80:80 -e OCL_API_HOST=${OCL_API_HOST} 485402728093.dkr.ecr.ap-southeast-1.amazonaws.com/bahmni-msf/ocl-frontend:latest
 
 echo "Cleanup..."
 docker system prune -a -f
