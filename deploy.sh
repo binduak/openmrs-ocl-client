14386f9acb440ad8b561435ce1e729c53b308a04
surajkumardas <surajkumar.das@thoughtworks.com>
2020-06-05 15:46:57 +0530

    Parameter added in deploy.sh & run.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 echo "Start the deploy.."
 OCL_API_HOST="$1"
+OCL_FRONTEND_IMG="$2"
 echo "Stop the running container.."
 docker stop ocl-frontend
 
@@ -9,7 +10,7 @@ echo "Remove the stopped container.."
 docker rm ocl-frontend
 
 echo "Run the latest docker.."
-docker run --name=ocl-frontend --restart unless-stopped -d -p 80:80 -e OCL_API_HOST=${OCL_API_HOST} 485402728093.dkr.ecr.ap-southeast-1.amazonaws.com/bahmni-msf/ocl-frontend:latest
+docker run --name=ocl-frontend --restart unless-stopped -d -p 80:80 -e OCL_API_HOST=${OCL_API_HOST} ${OCL_FRONTEND_IMG}:latest
 
 echo "Cleanup..."
 docker system prune -a -f
