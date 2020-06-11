bc0718b91baa2c3ded8f9b45a335469e76203be5
venkatavineela <vineela.vini421.vv@gmail.com>
2020-06-11 16:00:39 +0530

    Vinisha, Vineela | MOBN-1299 | Start tagging docker image with JIRA card number

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ OCL_FRONTEND_IMG="$2"
 export PATH=/usr/local/bin:$PATH
 echo $PATH
 export BUILD_TIME=$(date +"%Y-%m-%d-%H-%M-%S")
+export JIRA_CARD_NO='1'
 echo "Build time - $BUILD_TIME"
 echo "Docker build.."
 docker build --no-cache -t ocl-frontend:latest .
 docker tag ocl-frontend:latest ${OCL_FRONTEND_IMG}:latest
-docker tag ocl-frontend:latest ${OCL_FRONTEND_IMG}:$BUILD_TIME
+docker tag ocl-frontend:latest ${OCL_FRONTEND_IMG}:$JIRA_CARD_NO
 echo "aws ecr login .."
 aws ecr get-login-password --region ap-southeast-1|docker login --username AWS --password-stdin ${REPO_LOCATION}
 echo "Docker push to ECR.."
