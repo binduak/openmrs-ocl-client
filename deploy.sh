06056c39d7100a6a4321630edd92c885c5c11e5f
Vinisha Donthula <vinishad@thoughtworks.com>
2020-06-12 18:32:21 +0530

    Vinisha, Vineela | MOBN-1299 | Adding login in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,15 @@ echo "Start the deploy.."
 OCL_API_HOST="$1"
 OCL_FRONTEND_IMG="$2"
 DOCKER_IMAGE_TAG="$3"
+REPO_LOCATION="$4"
 echo "Stop the running container.."
 docker stop ocl-frontend
 
 echo "Remove the stopped container.."
 docker rm ocl-frontend
+
+echo "aws ecr login .."
+aws ecr get-login-password --region ap-southeast-1|docker login --username AWS --password-stdin ${REPO_LOCATION}
 
 echo "Run the latest docker.."
 docker run --name=ocl-frontend --restart unless-stopped -d -p 80:80 -e OCL_API_HOST=${OCL_API_HOST} ${OCL_FRONTEND_IMG}:${DOCKER_IMAGE_TAG}
