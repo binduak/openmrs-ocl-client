1224d3f7c37ad162d778fb8de8a5559af576e493
Kirity Mahanty <kirity.mahanty@thoughtworks.com>
2020-08-20 13:19:27 +0530

    Tarun, Kirity | MOBN-1390 | View the total number of concepts in a Source - Added active and retired concepts

--- src/apps/sources/components/SourceConceptsSummary.tsx
+++ src/apps/sources/components/SourceConceptsSummary.tsx
@@ -6,11 +6,18 @@ import {
   makeStyles,
   Paper,
   Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { retrieveConceptsAction } from "../../concepts/redux";
+import { StarBorder, DeleteForever } from "@material-ui/icons";
 
 interface Props {
   source?: APISource;
+  count: number;
 }
 
 const useStyles = makeStyles({
@@ -19,8 +26,12 @@ const useStyles = makeStyles({
   },
 });
 
-const SourceConceptsSummary: React.FC<Props> = ({ source }) => {
+const SourceConceptsSummary: React.FC<Props> = ({ source, count }) => {
   const classes = useStyles();
+
+  const total_concepts: number = count;
+  const active_concepts: number = source?.active_concepts || 0;
+  const retire_concepts: number = count - active_concepts;
 
   return (
     <Paper className='fieldsetParent'>
@@ -28,11 +39,22 @@ const SourceConceptsSummary: React.FC<Props> = ({ source }) => {
         <Typography component='legend' variant='h5' gutterBottom>
           Concepts
         </Typography>
-        <Typography variant='h6' data-testid='active-concepts' gutterBottom>
-          <b>{`Total Concepts: ${source?.active_concepts}`}</b>
-        </Typography>
         <Typography variant='h6' data-testid='active-mappings' gutterBottom>
-          <b>{`Total Mappings: ${source?.active_mappings}`}</b>
+          <b>{`Total Concepts: ${total_concepts}`}</b>
+          <List component='div' disablePadding>
+            <ListItem>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary={`Active Concepts: ${active_concepts}`} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <DeleteForever />
+              </ListItemIcon>
+              <ListItemText primary={`Retired Concepts: ${retire_concepts}`} />
+            </ListItem>
+          </List>
         </Typography>
         <ButtonGroup variant='text' fullWidth>
           <Button to={""} component={Link} color='primary'>
