2ced99e334a6e26c72fdb5566ef3cfdf0ce42451
supriyatw <45252490+supriyatw@users.noreply.github.com>
2020-08-27 18:53:29 +0530

    Bindu, Supriya | MOBN-1443 | Add limit to the User orgs API GET call (#32)

--- src/apps/authentication/api.ts
+++ src/apps/authentication/api.ts
@@ -4,7 +4,7 @@ const api = {
   login: (username: string, password: string) =>
     unAuthenticatedInstance.post("/users/login/", { username, password }),
   getProfile: () => authenticatedInstance.get("/user/"),
-  getUserOrgs: () => authenticatedInstance.get("/user/orgs/")
+  getUserOrgs: () => authenticatedInstance.get("/user/orgs/?limit=0")
 };
 
 export default api;
