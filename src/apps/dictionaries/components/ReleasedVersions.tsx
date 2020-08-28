86208c23c5bbdcabe71fb9e772bc61d1eeb32428
vinishad <40575981+vinishad@users.noreply.github.com>
2020-08-28 12:22:07 +0530

    MOBN-1441 | Make released version component of dictionary responsive and include date created field (#31)
    * Kirity | MOBN-1441 | Responsive issue for the released version component
    
    * Vinisha, Tarun | MOBN-1441 | Move view concepts and copy subscription url to actions
    
    * Vinisha, Tarun | MOBN-1441 | Add created date in release versions section
    
    * Vinisha, Tarun | MOBN-1441 | Updated few titles in Released versions section
    
    Co-authored-by: Kirity Mahanty <kirity.mahanty@thoughtworks.com>
    Co-authored-by: tarunkumar-tw <tarunkumar.s@thoughtworks.com>

--- src/apps/dictionaries/components/ReleasedVersions.tsx
+++ src/apps/dictionaries/components/ReleasedVersions.tsx
@@ -6,6 +6,7 @@ import {
     Dialog,
     Paper,
     Table,
+    TableContainer,
     TableBody,
     TableCell,
     TableHead,
@@ -13,12 +14,23 @@ import {
     Tooltip,
     Typography,
     Switch,
+    IconButton,
+    Menu,
+    MenuItem,
+    makeStyles,
+    Grid,
 } from "@material-ui/core";
+import {
+    MoreVert as MoreVertIcon,
+    FileCopy as FileCopyIcon,
+    Visibility as VisibilityIcon
+} from "@material-ui/icons";
 import { Link } from "react-router-dom";
-import {APIDictionaryVersion, DictionaryVersion} from "../types";
+import { APIDictionaryVersion, DictionaryVersion } from "../types";
 import DictionaryVersionForm from "./DictionaryVersionForm";
 import { BASE_URL } from "../../../utils";
 import ConfirmationDialog from "../../../utils/components/ConfirmationDialog";
+import moment from 'moment';
 
 interface Props {
   versions: APIDictionaryVersion[];
@@ -29,7 +41,18 @@ interface Props {
   createVersionError?: { detail: string };
   dictionaryUrl: string;
 }
-
+const useStyles = makeStyles({
+  container: {
+    maxHeight: 400,
+  },
+  buttonLink: {
+    textDecoration: "none",
+    color: "inherit",
+  },
+    addLeftPadding:{
+      paddingLeft: 5
+    },
+});
 const ReleasedVersions: React.FC<Props> = ({
   versions,
   showCreateVersionButton,
@@ -37,12 +60,17 @@ const ReleasedVersions: React.FC<Props> = ({
   editDictionaryVersion,
   createVersionLoading,
   createVersionError,
-  dictionaryUrl
+  dictionaryUrl,
 }) => {
-  const versionsToDisplay = versions.filter(row => row.id !== "HEAD");
-
+  const versionsToDisplay = versions.filter((row) => row.id !== "HEAD");
+  const [version, setVersion] = React.useState<DictionaryVersion>({
+    id: "",
+    released: false,
+    description: "",
+    external_id: "",
+  });
   const [open, setOpen] = React.useState(false);
-
+  const classes = useStyles();
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -51,124 +79,172 @@ const ReleasedVersions: React.FC<Props> = ({
   };
 
   const handleReleaseVersionChange = () => {
-     editDictionaryVersion({id:dictionaryVersion.id, released: !dictionaryVersion.released});
-     setConfirmDialogOpen(false);
+    editDictionaryVersion({
+      id: dictionaryVersion.id,
+      released: !dictionaryVersion.released,
+    });
+    setConfirmDialogOpen(false);
   };
 
   const openDialog = (row: APIDictionaryVersion) => {
-      setConfirmDialogOpen(true);
-      setDictionaryVersion(row);
+    setConfirmDialogOpen(true);
+    setDictionaryVersion(row);
   };
 
   const confirmationMsg = () => {
-      return(
-          <div >
-              <span>Are you sure to mark version </span>
-              <span style={{fontWeight: 'bold'}}>
-                    {dictionaryVersion.id}
-              </span> as {dictionaryVersion.released ? <span style={{color: '#f50057'}}>unreleased</span> :
-
-              <span style={{color: '#f50057'}}>released</span>}?
-          </div>
-      )
+    return (
+      <div>
+        <span>Are you sure to mark version </span>
+        <span style={{ fontWeight: "bold" }}>
+          {dictionaryVersion.id}
+        </span> as{" "}
+        {dictionaryVersion.released ? (
+          <span style={{ color: "#f50057" }}>unreleased</span>
+        ) : (
+          <span style={{ color: "#f50057" }}>released</span>
+        )}
+        ?
+      </div>
+    );
   };
 
   const [confirmDialogOpen, setConfirmDialogOpen] = React.useState(false);
-
-  const [dictionaryVersion, setDictionaryVersion] = React.useState<DictionaryVersion>(
-    {
-        id: "",
-        released: false,
-        description: "",
-        external_id: ""
-    });
-
-    return (
-    <Paper className="fieldsetParent">
-      <fieldset style={{minWidth: "0"}}>
-        <Typography component="legend" variant="h5" gutterBottom>
-          Releases
-        </Typography>
-        {versionsToDisplay.length > 0 ? (
-          <div>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Concepts</TableCell>
-                  <TableCell>Subscription URL</TableCell>
-                  <TableCell>Release Status</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {versionsToDisplay.map((row: APIDictionaryVersion) => (
-                  <TableRow key={row.id}>
-                    <TableCell  style={{wordBreak: 'break-all'}}>{row.id}</TableCell>
-                    <TableCell style={{wordBreak: 'break-all'}}>{row.description || "None"}</TableCell>
-                    <TableCell>
-                      <Button
-                        // not row.url because the response immediately after creating a new version is missing the url attribute for some reason
-                        to={`${dictionaryUrl}${row.id}/concepts/`}
-                        component={Link}
-                        size="small"
-                        variant="text"
-                        color="primary"
-                      >
-                        View
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <CopyToClipboard text={`${BASE_URL}${dictionaryUrl}${row.id}/`}>
-                        <Tooltip title={`${BASE_URL}${dictionaryUrl}${row.id}/`}>
-                          <Button size="small" variant="text" color="primary">
-                            Copy
-                          </Button>
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [dictionaryVersion, setDictionaryVersion] = React.useState<
+    DictionaryVersion
+  >({
+    id: "",
+    released: false,
+    description: "",
+    external_id: "",
+  });
+  const handleClick = (
+      event: React.MouseEvent<HTMLElement>,
+      version: DictionaryVersion
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setVersion(version);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+  return (
+      <Paper className='fieldsetParent'>
+        <fieldset style={{ minWidth: "0" }}>
+          <Typography component='legend' variant='h5' gutterBottom>
+            Versions
+          </Typography>
+          {versionsToDisplay.length > 0 ? (
+              <TableContainer className={classes.container}>
+                <Table stickyHeader>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ID</TableCell>
+                      <TableCell>Date Created</TableCell>
+                      <TableCell>Description</TableCell>
+                      <TableCell>Release Status</TableCell>
+                      <TableCell>Actions</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {versionsToDisplay.map((row: APIDictionaryVersion) => (
+                        <TableRow key={row.id}>
+                          <TableCell>{row.id}</TableCell>
+                          <TableCell>{row.created_on ? moment(row.created_on).format("DD MMM YYYY") : ""}</TableCell>
+                          <TableCell style={{ wordBreak: "break-all" }}>
+                            {row.description || "None"}
+                          </TableCell>
+                          <TableCell>
+                            {showCreateVersionButton ? (
+                                <Switch
+                                    data-testid={row.id}
+                                    checked={row.released}
+                                    onChange={() => openDialog(row)}
+                                    name='checkReleaseStatus'
+                                    color='primary'
+                                />
+                            ) : (
+                                <Tooltip title="You don't have permission to change the status">
+                          <Switch
+                            data-testid={row.id}
+                            checked={row.released}
+                            name='checkReleaseStatus'
+                            disableRipple={true}
+                            color='primary'
+                            style={{
+                              cursor: "default",
+                              opacity: 1,
+                              backgroundColor: "transparent",
+                            }}
+                          />
                         </Tooltip>
-                      </CopyToClipboard>
+                      )}
                     </TableCell>
-                      <TableCell>
-                          {showCreateVersionButton ?
-                              <Switch
-                              data-testid={row.id}
-                              checked={row.released}
-                              onChange={() => openDialog(row)}
-                              name="checkReleaseStatus"
-                              color="primary"
-                              /> :
-                              <Tooltip title="You don’t have permission to change the status">
-                                  <Switch
-                                      data-testid={row.id}
-                                      checked={row.released}
-                                      name="checkReleaseStatus"
-                                      disableRipple={true}
-                                      color="primary"
-                                      style={{cursor: "default", opacity: 1, backgroundColor: "transparent"}}
-                                  />
-                              </Tooltip>}
-                      </TableCell>
+                    <TableCell>
+                      <Tooltip title='More actions' enterDelay={700}>
+                        <IconButton
+                          data-testid={"more-actions"}
+                          aria-label='more'
+                          aria-controls='menu'
+                          aria-haspopup='true'
+                          onClick={(e) => handleClick(e, row)}
+                        >
+                          <MoreVertIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Menu
+                        id='long-menu'
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleCloseMenu}
+                      >
+                          <MenuItem onClick={handleCloseMenu} component={Link}
+                                    to={`${dictionaryUrl}${version.id}/concepts/`}>
+                              <Grid data-testid={"view-concepts"}>
+                              <VisibilityIcon fontSize={"small"}/>
+                                  <span className={classes.addLeftPadding}> View Concepts</span>
+                              </Grid>
+                          </MenuItem>
+                        {!version.released ? null : (
+                          <MenuItem onClick={handleCloseMenu}>
+                            <CopyToClipboard
+                              text={`${
+                                version.released
+                                  ? `${BASE_URL}${dictionaryUrl}${version.id}/`
+                                  : null
+                              }`}
+                            >
+                                <Grid data-testid={"copy-subscription-url"}>
+                                <FileCopyIcon fontSize={"small"} />
+                                <span className={classes.addLeftPadding}>Copy Subscription URL</span>
+                                </Grid>
+                            </CopyToClipboard>
+                          </MenuItem>
+                        )}
+                      </Menu>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
-          </div>
+          </TableContainer>
         ) : (
-          <Typography align="center">No released versions</Typography>
+          <Typography align='center'>No versions created</Typography>
         )}
         <br />
         {!showCreateVersionButton ? null : (
-          <ButtonGroup fullWidth variant="text" color="primary">
-            <Button onClick={handleClickOpen}>Release new version</Button>
+          <ButtonGroup fullWidth variant='text' color='primary'>
+            <Button onClick={handleClickOpen}>Create new version</Button>
           </ButtonGroup>
         )}
       </fieldset>
       <ConfirmationDialog
-          open={confirmDialogOpen}
-          setOpen={setConfirmDialogOpen}
-          onConfirm={() => handleReleaseVersionChange()}
-          message={confirmationMsg()}
-          cancelButtonText={"No"}
-          confirmButtonText={"Yes"}
+        open={confirmDialogOpen}
+        setOpen={setConfirmDialogOpen}
+        onConfirm={() => handleReleaseVersionChange()}
+        message={confirmationMsg()}
+        cancelButtonText={"No"}
+        confirmButtonText={"Yes"}
       />
       <Dialog onClose={handleClose} open={open}>
         <DictionaryVersionForm
